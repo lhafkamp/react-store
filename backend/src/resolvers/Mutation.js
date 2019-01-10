14024ef8d005a287de3c2c00b5f19669be5c4256
Luuk <luukhafkamp@gmail.com>
2019-01-10 17:44:08 +0100

    Added updateItem resolver

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -7,13 +7,21 @@ const Mutations = {
 		}, info);
 
 		return item;
+	},
+	
+	updateItem(parent, args, ctx, info) {
+		// first take a copy of the updates
+		const updates = { ...args };
+		// remove the ID from the updates
+		delete updates.id;
+		// run the update method
+		return ctx.db.mutation.updateItem({
+			data: updates,
+			where: {
+				id: args.id
+			},
+		}, info);
 	}
-	// createDog(parent, args, ctx, info) {
-	// 	global.dogs = global.dogs || []
-	// 	const newDog = { name: args.name };
-	// 	global.dogs.push(newDog);
-	// 	return newDog;
-	// }
 };
 
 module.exports = Mutations;
