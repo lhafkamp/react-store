3ae5ba683f624ee5841af38e7715d656106808dc
Luuk <luukhafkamp@gmail.com>
2019-02-27 16:46:53 +0100

    Update and delete items

--- frontend/components/Item.js
+++ frontend/components/Item.js
@@ -7,6 +7,8 @@ import ItemStyles from './styles/ItemStyles';
 import PriceTag from './styles/PriceTag';
 import formatMoney from '../lib/formatMoney';
 
+import DeleteItem from './DeleteItem';
+
 class Item extends Component {
 	static propTypes = {
 		item: PropTypes.object.isRequired,
@@ -24,7 +26,7 @@ class Item extends Component {
 						}}>
 						<a>{item.title}</a>
 					</Link>
-				</Title>
+				</Title>A
 				<PriceTag>{formatMoney(item.price)}</PriceTag>
 				<p>{item.description}</p>
 
@@ -36,7 +38,7 @@ class Item extends Component {
 						<a>Edit ✏️</a>
 					</Link>
 					<button>Add To Cart</button>
-					<button>Delete</button>
+					<DeleteItem id={item.id}>Delete This Item</DeleteItem>
 				</div>
 			</ItemStyles>
 		)
